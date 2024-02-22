69a5c77e8cbaa5f8fcc6fe1397be4ec2c89267e2
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2024-02-22 12:19:46 +0400

    done

---
+++ ASP.NET-Intro/ASP.NET-Intro/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using ASP.NET_Intro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Intro.Controllers;
+
+public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product() { Id = 1, Name = "Playstation 4 Slim", Price = 450, Description = "Ela veziyyetdedir"},
+            new Product() { Id = 2, Name = "Et ceken mashin", Price = 400, Description = "Anlatmaya gerek yok"},
+            new Product() { Id = 3, Name = "Mikrodalga", Price = 1000, Description = "Agillidir."},
+        };
+
+        return View(products);
+    }
+
+    //public IActionResult Test()
+    //{
+    //    //List<string> productNames = new List<string>()
+    //    //{
+    //    //    "Playstation 4 Slim", "Et ceken mashin", "Mikro dalgali soba"
+    //    //};
+    //    string name = "Kamal";
+    //    TempData["Name"] = name;
+
+    //    return RedirectToAction("Index");
+    //}
+
+    public IActionResult Detail(int id)
+    {
+        //ViewBag.Id = id;
+        //ViewData["id"] = id;
+        TempData["id"] = id;
+
+        return View();
+    }
+}
---
+++ ASP.NET-Intro/ASP.NET-Intro/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_Intro.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+}
